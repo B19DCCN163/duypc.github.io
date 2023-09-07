fa091a32e6b9594466e868f6eaccdd7a955bb1c5
B19DCCN163 <duydan0705@gmail.com>
2023-09-07 21:46:07 +0700

    Denoe

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Select from 'react-select';
 import './App.css';
-import  loamini from './img/loamini.jpg'
-import phim from './img/phim.jpg'
+import loamini from './img/loamini.jpg'
+import phim from './img/phim.jpeg'
 import lotchuot from './img/lotchuot.jpg'
-import {dataCard, dataCase, dataLai, dataMainchip, dataManhinh, dataPow, dataRam, dataSsd} from './Data'
+import { dataCard, dataCase, dataLai, dataMainchip, dataManhinh, dataPow, dataRam, dataSsd, dataTan } from './Data'
 
 interface Computer {
   mainchip: string | undefined;
@@ -15,7 +15,8 @@ interface Computer {
   pow: string | undefined;
   case: string | undefined;
   manhinh: string | undefined;
-  lai : string | undefined;
+  lai: string | undefined;
+  tan: string | undefined;
 
 }
 
@@ -38,49 +39,150 @@ function App() {
     pow: '400w',
     case: 'Rainbow Đen + Fan Led',
     manhinh: 'Vsp 22 inch Full Vien 75hz',
-    lai : '1200'
+    lai: '1400',
+    tan: 'Tản Khí AM 6100'
 
   })
   useEffect(() => {
-    let priceMainchip : any = dataMainchip.find((value, index) => {
+    let priceMainchip: any = dataMainchip.find((value, index) => {
       return value.values === computer.mainchip
     })
-    let priceRam : any = dataRam.find((value, index) => {
+    let priceRam: any = dataRam.find((value, index) => {
       return value.values === computer.ram
     })
-    let priceCard : any = dataCard.find((value, index) => {
+    let priceCard: any = dataCard.find((value, index) => {
       return value.values === computer.card
     })
-    let priceSsd : any = dataSsd.find((value, index) => {
+    let priceSsd: any = dataSsd.find((value, index) => {
       return value.values === computer.ssd
     })
-    let priceManhinh : any = dataManhinh.find((value, index) => {
+    let priceManhinh: any = dataManhinh.find((value, index) => {
       return value.values === computer.manhinh
     })
-    let priceCase : any = dataCase.find((value, index) => {
+    let priceCase: any = dataCase.find((value, index) => {
       return value.values === computer.case
     })
-    let pricePow : any = dataPow.find((value, index) => {
+    let pricePow: any = dataPow.find((value, index) => {
       return value.values === computer.pow
     })
-    let priceLai : any = dataLai.find((value, index) => {
+    let priceTan: any = dataTan.find((value, index) => {
+      return value.values === computer.tan
+    })
+    let priceLai: any = dataLai.find((value, index) => {
       return value.values === computer.lai
     })
-    setPriceOld(500 + priceMainchip?.price + priceRam?.price + priceCard?.price + priceSsd?.price + priceManhinh?.price + pricePow?.price + priceCase?.price)
-    setPrice(priceold + priceLai?.price )
+    setPriceOld(175 + 50 + 250 + priceTan?.price + priceMainchip?.price + priceRam?.price + priceCard?.price + priceSsd?.price + priceManhinh?.price + pricePow?.price + priceCase?.price)
+    setPrice(priceold + priceLai?.price)
     setImg(priceCase.img)
   })
-
   return (
     <div className="App">
+      <div className='header'>
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H61 - i5 2400',
+          ram: '8 Gb Buss 1600',
+          ssd: '120GB',
+          card: '650Ti',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai: '1400',
+          tan: 'Tản Khí AM 6100'
+        })}>6000</button>
+
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H81 - i5 4570',
+          ram: '8 Gb Buss 1600',
+          ssd: '256GB',
+          card: '750Ti',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai : '1400',
+          tan : 'Tản Khí AM 6100'
+        })}>Valorant - 6tr4</button>
+        
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H81 - i5 4570',
+          ram: '8 Gb Buss 1600',
+          ssd: '256GB',
+          card: '960 4Gb',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai : '1400',
+          tan : 'Tản Khí AM 6100'
+        })}>Genshin - 6tr7</button>
+
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H81 - Intel  Xeon 1220v3',
+          ram: '8 Gb Buss 1600',
+          ssd: '120GB',
+          card: '750Ti',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai: '1400',
+          tan: 'Tản Khí AM 6100'
+        })}>Xeon - 7tr</button>
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H110 - i5 6500',
+          ram: '16 Gb Buss 1600',
+          ssd: '256GB',
+          card: '1060 3Gb',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai : '1400',
+          tan : 'Tản Khí AM 6100'
+        })}>GTA - 7tr8</button>
+        <button className='btn-set' onClick={() => setComputer({
+           mainchip: 'H310 - i3 9100',
+           ram: '16 Gb Buss 1600',
+           ssd: '256GB',
+           card: '1060 3Gb',
+           pow: '400w',
+           case: 'Rainbow Đen + Fan Led',
+           manhinh: 'Vsp 22 inch Full Vien 75hz',
+           lai : '1400',
+           tan : 'Tản Khí AM 6100'
+        })}>Pubg - 8tr4</button>
+
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'H510 - i3 10100',
+          ram: '16 Gb Buss 2666',
+          ssd: '256GB',
+          card: '1060 3Gb',
+          pow: '400w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 22 inch Full Vien 75hz',
+          lai: '1400',
+          tan: 'Tản Khí AM 6100'
+        })}>10000</button>
+
+
+        <button className='btn-set' onClick={() => setComputer({
+          mainchip: 'B660 - i5 12400',
+          ram: '16 Gb Buss 3200',
+          ssd: '512GB',
+          card: '2060',
+          pow: '550w',
+          case: 'Rainbow Đen + Fan Led',
+          manhinh: 'Vsp 27 inch Full Vien 75hz',
+          lai: '1400',
+          tan: 'Tản Khí AM 6100'
+        })}>16200</button>
+        
+
+        
+      </div>
       <div className='myapp'>
         <div className='build'>
           <div className='div_1'>
-            <p className='p_div_1'>MAIN - CHIP</p>
             <Select
               className='input_div_12'
               options={dataMainchip}
-              value={{ values: computer.mainchip, label : computer.mainchip}}
+              value={{ values: computer.mainchip, label: computer.mainchip }}
               onChange={e => setComputer({
                 ...computer,
                 mainchip: e?.values
@@ -88,7 +190,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>RAM</p>
             <Select
               className='input_div_12'
               options={dataRam}
@@ -100,7 +201,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>Ổ Cứng SSD - HDD</p>
             <Select
               className='input_div_12'
               options={dataSsd}
@@ -112,7 +212,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>Card Màn Hình</p>
             <Select
               className='input_div_12'
               options={dataCard}
@@ -124,7 +223,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>Nguồn</p>
             <Select
               className='input_div_12'
               options={dataPow}
@@ -136,7 +234,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>Vỏ Case</p>
             <Select
               className='input_div_12'
               options={dataCase}
@@ -148,7 +245,6 @@ function App() {
             />
           </div>
           <div className='div_1'>
-            <p className='p_div_1'>Màn Hình</p>
             <Select
               className='input_div_12'
               options={dataManhinh}
@@ -159,60 +255,63 @@ function App() {
               })}
             />
           </div>
-         
-          </div>
-          <div className='showout'>
-          <div className='showimg'>
-            <div className='showimg0' > <img style={{width : '100% ', height : 'auto'}} src= {img} /></div>
-            <div className='showimg1' > <img style={{width : '100% ', height : 'auto'}} src={loamini} /></div>
-            <div className='showimg1' > <img style={{width : '100% ', height : '140%', transform: 'rotate(270deg)'}} src={phim} /></div>
-            <div className='showimg1' > <img style={{width : '90% ', height : 'auto'}} src={lotchuot} /></div>
-            <p className='giakhach'> Giá : { (price - price%1000) / 1000}.{price%1000}.000</p>
-            </div>
-            <div className='showcase'>
-              <p className='showP'>{computer.mainchip}</p>
-              <p className='showP'>Ram {computer.ram}</p>
-              <p className='showP'>Ổ Cứng Ssd {computer.ssd}</p>
-              <p className='showP'>Card Gtx {computer.card}</p>
-              <p className='showP'>Nguồn {computer.pow}</p>
-              <p className='showP'> Vỏ {computer.case}</p>
-              <p className='showP'>Màn hình {computer.manhinh}</p>
-              <p className='showPcolor'>Tặng kèm Phím Chuột, Lót Chuột, Loa Mini, Dây Mạng, ...</p>
-              <p className='showPcolorBlue'>Sdt/Zalo : 0387 475 071 - Duy Pc Gaming</p>
-  
-  
-            </div>
-            
-          </div>
-        </div>
-
-        
-       <p className='tinhgiagoc'> Giá không lãi : {priceold}</p>\
-       <div className='div_1'>
+          <div className='div_1'>
             <Select
-              className='input_div_12lai'
-              options={dataLai}
-              value={{ values: computer.lai, label: computer.lai }}
+              className='input_div_12'
+              options={dataTan}
+              value={{ values: computer.tan, label: computer.tan }}
               onChange={e => setComputer({
                 ...computer,
-                lai: e?.values
+                tan: e?.values
               })}
             />
           </div>
-        {/* <button className='btn-tinh' onClick={handleClick}>TINH </button> */}
+
+        </div>
+        <div className='showout'>
+          <div className='showimg'>
+            <div className='showimg0' > <img style={{ width: '100% ', height: 'auto' }} src={img} /></div>
+            <div className='showimg1' > <img style={{ width: '100% ', height: 'auto' }} src={loamini} /></div>
+            <div className='showimg1' > <img style={{ width: '100% ', height: '140%', transform: 'rotate(270deg)' }} src={phim} /></div>
+            <div className='showimg1' > <img style={{ width: '90% ', height: 'auto' }} src={lotchuot} /></div>
+            <p className='giakhach'> Giá : {(price - price % 1000) / 1000}.{price % 1000}.000</p>
+          </div>
+          <div className='showcase'>
+            <p className='showP'>{computer.mainchip}</p>
+            <p className='showP'>Ram {computer.ram}</p>
+            <p className='showP'>Ổ Cứng Ssd {computer.ssd}</p>
+            <p className='showP'>Card Gtx {computer.card}</p>
+            <p className='showP'>Nguồn {computer.pow}</p>
+            <p className='showP'> Vỏ {computer.case}</p>
+            <p className='showP'>Màn hình {computer.manhinh}</p>
+            <p className='showPcolor'>Tặng kèm Phím Chuột, Lót Chuột, Loa Mini, Dây Mạng, ...</p>
+            <p className='showPcolorBlue'>Sdt/Zalo : 0387 475 071 - Duy Pc Gaming</p>
+
+
+          </div>
+
+        </div>
       </div>
-    );
-  }
-  
-  export default App;
 
 
+      <p className='tinhgiagoc'> Giá không lãi : {priceold}</p>\
+      <div className='div_1'>
+        <Select
+          className='input_div_12lai'
+          options={dataLai}
+          value={{ values: computer.lai, label: computer.lai }}
+          onChange={e => setComputer({
+            ...computer,
+            lai: e?.values
+          })}
+        />
+      </div>
+      {/* <button className='btn-tinh' onClick={handleClick}>TINH </button> */}
+    </div>
+  );
+}
 
-
-
-
-
-
+export default App;
 
 
 
